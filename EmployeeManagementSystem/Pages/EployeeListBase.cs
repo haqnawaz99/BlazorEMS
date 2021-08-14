65688ff6548844de8ead341ba9635c436ed162d3
Haq Nawaz <haqnawaz99@yahoo.com>
2021-08-14 20:30:25 +0500

    Database Update
    Database migration updated

--- EmployeeManagementSystem/Pages/EployeeListBase.cs
+++ EmployeeManagementSystem/Pages/EployeeListBase.cs
@@ -28,7 +28,7 @@ namespace EmployeeManagementSystem.Pages
                 Email = "David@pragimtech.com",
                 DateOfBrith = new DateTime(1980, 10, 5),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
                 PhotoPath = "images/1.jpg"
             };
 
@@ -40,7 +40,7 @@ namespace EmployeeManagementSystem.Pages
                 Email = "Sam@pragimtech.com",
                 DateOfBrith = new DateTime(1981, 12, 22),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+                DepartmentId = 2,
                 PhotoPath = "images/2.jpg"
             };
 
@@ -52,7 +52,7 @@ namespace EmployeeManagementSystem.Pages
                 Email = "mary@pragimtech.com",
                 DateOfBrith = new DateTime(1979, 11, 11),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 3,
                 PhotoPath = "images/3.jpg"
             };
 
@@ -64,7 +64,7 @@ namespace EmployeeManagementSystem.Pages
                 Email = "sara@pragimtech.com",
                 DateOfBrith = new DateTime(1982, 9, 23),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                DepartmentId = 4,
                 PhotoPath = "images/4.jpeg"
             };
 
