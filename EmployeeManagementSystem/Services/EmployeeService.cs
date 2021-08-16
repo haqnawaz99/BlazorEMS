d5483401695f485e7acc8049b5b5aee102e116df
Haq Nawaz <haqnawaz99@yahoo.com>
2021-08-16 21:20:29 +0500

    REST API from Blazor
    Call REST API from ASP.NET Core Blazor

---
+++ EmployeeManagementSystem/Services/EmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+
+namespace EmployeeManagementSystem.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly HttpClient httpClient;
+
+        public EmployeeService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await httpClient.GetJsonAsync<Employee[]>("api/employees");
+           
+        }
+    }
+}
